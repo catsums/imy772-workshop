6a45866d120587daf97fb0fbe77440a378fe0e91
Catsim <111745304+catsums@users.noreply.github.com>
2024-05-18 18:54:01 +0200

    Added HEX to DEC

--- src/processing.ts
+++ src/processing.ts
@@ -1,9 +1,12 @@
 import _ from 'lodash';
+import { InvalidHexCalcError, NegativeValueCalcError } from './calc_errors';
 
 export const HEX_CHARS = "0123456789ABCDEF";
 
 const inputLimit = 3;
 const outputLimit = 6;
+
+const hexBase = 16;
 
 export function parseInput(input: string): string {
 	let parsed = "";
@@ -35,5 +38,41 @@ export function parseOutput(output: string){
 	parsed = parsed.substring(parsed.length - outputLimit);
 
 	return parsed;
+
+}
+
+export function HEXtoDEC(hexString: string): string {
+	let sum = 0;
+	
+	//use uppercase
+	hexString = hexString.toUpperCase();
+
+	//throw error if HEX is negative
+	if(hexString[0] === '-'){
+		throw new NegativeValueCalcError(`${hexString} can't be processed because it's negative`, hexString);
+	}
+
+	//take integral number only
+	hexString = hexString.split(".")[0];
+
+	for(let i=0; i<hexString.length; i++){
+		let index = (hexString.length - 1) - i;
+		let hexDigit = HEX_CHARS.indexOf(hexString[i]);
+
+		//throw error for invalid HEX digit
+		if(hexDigit < 0){
+			throw new InvalidHexCalcError(`${hexString} can't be processed because it's not a valid HEX value`, hexString);
+		}
+		let val = Math.pow(hexBase, index) * hexDigit;
+
+		sum += val;
+	}
+
+	//throw error if sum is (somehow) negative
+	if(sum < 0){
+		throw new NegativeValueCalcError(`${sum} can't be processed because it became negative (somehow)`, sum);
+	}
+
+	return sum.toString();
 
 }
