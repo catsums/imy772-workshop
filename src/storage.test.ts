4b273de60971929c9ad5884afebf6f80a9879871
Catsim <111745304+catsums@users.noreply.github.com>
2024-05-18 23:15:14 +0200

    Updated tests and added error handling

--- src/storage.test.ts
+++ src/storage.test.ts
@@ -1,11 +1,11 @@
 
 import {describe, test, expect, jest} from "@jest/globals";
 
-let mockAddToken = jest.fn((x, store) => {});
-let mockCalculate = jest.fn((store) => "0");
+import { processStore, addTokens, addToken } from "./storage";
+import { CalcError } from "./calc_errors";
 
 describe("Test Storage from input stream", () => {
-	test("Test inserting digit tokens", () => {
+	describe("Test inserting digit tokens", () => {
 		let tests = {
 			"1" : ["1"],
 			"0" : ["0"],
@@ -16,8 +16,8 @@ describe("Test Storage from input stream", () => {
 			"00100" : ["100"],
 			"1.23" : ["123"],
 			"3.14167" : ["314"],
-			"" : [""],
-			"you" : [""],
+			"" : [],
+			"you" : [],
 			"prime" : ["E"],
 			"p0t" : ["0"],
 			"A.bc" : ["ABC"],
@@ -25,52 +25,66 @@ describe("Test Storage from input stream", () => {
 		};
 
 		for(let [k,v] of Object.entries(tests)){
-			let store = [];
-			let actual = mockAddToken(k, store);
-
-			expect(actual).toEqual(v);
+			test(`Test addTokens(${k})`, () => {
+				let actual = addTokens(k, []);
+				
+				expect(actual).toEqual(v);
+			});
 		}
 	});
 
-	test("Test inserting operator tokens", () => {
+	describe("Test inserting operator tokens", () => {
 		let tests = {
 			"1+" : ["1","+"],
 			"0-" : ["0","-"],
 			"*" : [],
 			"-1" : ["1"],
 			"A1+2" : ["A1","+","2"],
-			"1+2+3" : ["1","2","3"],
-			"05-004-0003-002-01" : ["5","4","3","2","1"],
+			"1+2+3" : ["1","+","2","+","3"],
+			"05-004-0003-002-01" : ["5","-","4","-","3","-","2","-","1"],
 			"5+-" : ["5","-"],
 			"1//" : ["1","/"],
-			"1/3+9.2-5" : ["1","/","3","+","9","-","5"],
+			"1/3+0.9-5" : ["1","/","3","+","9","-","5"],
 			"3.-+-+-+" : ["3","+"],
 			"Y0U-ARE-A-F1SH" : ["0","-","AE","-","A","-","F1"],
 		};
 
 		for(let [k,v] of Object.entries(tests)){
-			let store = [];
-			let actual = mockAddToken(k, store);
+			test(`Test addTokens(${k})`, () => {
+				let actual = addTokens(k, []);
 
-			expect(actual).toEqual(v);
+				expect(actual).toEqual(v);
+			});
 		}
 	});
 
-	test("Test calculating stores", () => {
+	describe("Test calculating stores", () => {
 		let tests = {
 			"1+1": "2",
 			"1+9": "A",
 			"10-1": "F",
 			"5*4": "14",
 			"1+2-1": "2",
+			"1+1+1+1*8": "B",
 			"A/B+1": "2",
+			"1+2*5-3/7*3+9": "13",
 		};
 
 		for(let [k,v] of Object.entries(tests)){
-			let store = [];
-			let actual = mockAddToken(k, store);
-
-			expect(actual).toEqual(v);
+			test(`Test processStore([${k}])`, () => {
+				try{
+					let store = addTokens(k, []);
+					let actual = processStore(store);
+	
+					expect(actual).toBe(v);
+				}catch(err){
+					if(err instanceof CalcError){
+						expect(err).toBeInstanceOf(v);
+					}else{
+						throw err;
+					}
+				}
+			});
 		}
 	});
 });
