92c8c19471446d0e1e228ac5aae5620538d0414c
Catsim <111745304+catsums@users.noreply.github.com>
2024-05-18 18:42:50 +0200

    Added processing for output

--- src/processing.ts
+++ src/processing.ts
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 
-let HEX_CHARS = "0123456789ABCDEF";
+export const HEX_CHARS = "0123456789ABCDEF";
 
 const inputLimit = 3;
+const outputLimit = 6;
 
 export function parseInput(input: string): string {
 	let parsed = "";
 
+	//filter out invalid characters
 	for(let char of input){
 		if(HEX_CHARS.includes(char.toUpperCase())){
 			parsed += char;
@@ -17,4 +19,21 @@ export function parseInput(input: string): string {
 	parsed = parsed.substring(0, inputLimit);
 
 	return parsed;
+}
+
+export function parseOutput(output: string){
+	let parsed = "";
+
+	//filter out invalid characters
+	for(let char of output){
+		if(HEX_CHARS.includes(char.toUpperCase())){
+			parsed += char;
+		}
+	}
+
+	//limit number of output characters (right to left)
+	parsed = parsed.substring(parsed.length - outputLimit);
+
+	return parsed;
+
 }
