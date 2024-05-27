5c368b6622fb25c1a7e51d8a7b25b01455c48d4d
Catsim <111745304+catsums@users.noreply.github.com>
2024-05-27 12:19:50 +0200

    Added Sass globals

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from './assets/vite.svg'
+import s from './css/p.module.scss'
+import './css/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={s.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={[s.logo, 'react'].join(' ')} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -25,10 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
