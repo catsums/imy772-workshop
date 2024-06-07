711b25edd3ebb7a485c97297a048b28d0842c534
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-07 23:38:16 +0200

    Update e2e.test.ts

--- src/e2e.test.ts
+++ src/e2e.test.ts
@@ -1,10 +1,15 @@
 
+import {render, screen, fireEvent} from '@testing-library/react'
 import {app, port} from "../server";
 import request from 'supertest';
+import {JSDOM} from "jsdom";
 
-describe("AAAAAA", () => {
-	test("Huh", async()=>{
-		let res = await request(app).get("/");
+test("Test request from server", () => {
+	let res = await request(app).get("/");
 		
-	})
+	let dom = new JSDOM(res.text);
+	let calc = dom.window.document.querySelector(".calculator");
+
+	expect(calc).toBeTruthy();
+	
 });
