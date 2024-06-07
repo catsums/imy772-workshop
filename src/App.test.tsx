a38add7bf2771818b6067e6ea2a52934bd853e66
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-07 22:25:07 +0200

    Added happy dom to only App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,7 @@
+/// <reference lib="dom" />
+
+import "../happydom"
+
 import {render, screen, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 // import '@testing-library/jest-dom'
